724abece19dc546b5d9ec7826d4bdc5108467c8d
jianke1991 <huijian1991@gmail.com>
2018-02-23 22:57:57 +0800

    Merge branch 'dev' into dev-whj
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/main/java/com/grid/base/BaseObject.java
@@ -0,0 +1,11 @@
+package com.grid.base;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Created by hanxingwang on 2017/12/18.
+ */
+public class BaseObject {
+    protected final Log log = LogFactory.getLog(getClass());
+}
